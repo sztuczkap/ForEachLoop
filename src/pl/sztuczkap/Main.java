d5770d6ac3755240ee9db52312bd111185139a62
sztuczkap <p.sztuczka@wp.pl>
2020-12-28 20:15:34 +0100

    funkcja forEach

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,11 +1,31 @@
 package pl.sztuczkap;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
+/*        // metoda of przed java 9
+        List<String> names = new ArrayList<>();
+        names.add();
+        names.add();
+        names.add();*/
+
+        List<String> names = List.of("Ania", "Maria", "Sebastian", "Tadeusz");
+
+        names.forEach(name -> {
+            if (name.length() < 5) {
+                System.out.println(name);
+            }
+        });
+
+        // przetwarzamy elementy mapy
+        Map<Integer, String> personMap = Map.of(1, "Michał", 2, "Ola", 3, "Marta");
+        personMap.forEach((key, value) -> System.out.println(".forEach(): Index: " + key + " Imię: " + value));
+        personMap.entrySet().forEach(entry -> System.out.println(".entrySet(): Index: " + entry.getKey() + " Imię: " + entry.getValue()));
 
     }
 }
